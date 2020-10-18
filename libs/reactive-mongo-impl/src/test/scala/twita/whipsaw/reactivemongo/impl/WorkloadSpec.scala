7be72df036dffa0d9b91f902d420ec63e38c50f6
Brian Lawler <bplawler@gmail.com>
2020-10-17 23:48:37 -0600

    Cleared out the bit about WorkloadRegistry - for now I'm going on the assumption that we need to know the types at compile time.

--- libs/reactive-mongo-impl/src/test/scala/twita/whipsaw/reactivemongo/impl/WorkloadSpec.scala
+++ libs/reactive-mongo-impl/src/test/scala/twita/whipsaw/reactivemongo/impl/WorkloadSpec.scala
@@ -1,6 +1,5 @@
 package twita.whipsaw.reactivemongo.impl
 
-import enumeratum._
 import org.scalatest.flatspec._
 import org.scalatest.matchers._
 import twita.dominion.api.DomainObjectGroup
@@ -15,35 +14,12 @@ import twita.whipsaw.api.Workloads
 import scala.concurrent.Future
 
 package test {
-  import twita.dominion.api.DomainObjectGroup
   import twita.dominion.impl.reactivemongo.MongoContext
-
   import scala.concurrent.ExecutionContext
-
-  /*
-  object WorkloadRegistry extends Enum[WorkloadRegistryEntry] {
-    import scala.concurrent.ExecutionContext.Implicits.global
-
-    implicit lazy val mongoContext = new DevMongoContextImpl
-    val values = findValues
-
-    case object Sample extends WorkloadRegistryEntry(new SampleMongoWorkloadFactory())
-
-    private lazy val workloadFactory = new MongoWorkloads[String]
-    def byId[Payload](id: WorkloadId): Future[Option[Workloads[Payload]]] = workloadFactory.get(DomainObjectGroup.byId(id)).map { wlOpt =>
-      wlOpt.map(wl => withName(wl.factoryType).entry.asInstanceOf[Workloads[Payload]])
-    }
-    def byFactoryType(factoryType: String): Workloads[_] = withName(factoryType).entry
-  }
-  */
 
   class SampleMongoWorkloadFactory(implicit executionContext: ExecutionContext, mongoContext: MongoContext)
   extends MongoWorkloads[test.SamplePayload] {
     override def eventLogger: EventLogger = new MongoObjectEventStackLogger(4)
-  }
-
-  sealed class WorkloadRegistryEntry(val entry: Workloads[_]) extends EnumEntry {
-    override def entryName: String = entry.getClass.getName
   }
 }
 
@@ -66,7 +42,7 @@ class WorkloadSpec extends AsyncFlatSpec with should.Matchers {
     val factory = new test.SampleMongoWorkloadFactory()
     for {
       workload <- factory.get(DomainObjectGroup.byId(workloadId))
-      createdItem <- workload.get.workItems.apply(
+      createdItem <- workload.get.workItems(
         WorkItems.WorkItemAdded(test.SamplePayload("bplawler@gmail.com", "string to be processed"))
       )
     } yield assert(createdItem.id != null)
