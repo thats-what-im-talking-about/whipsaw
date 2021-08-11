2610d0d2f73534b2b904845e176f99a68633c681
Brian Lawler <brian@iterable.com>
2021-08-11 10:29:43 -0600

    reformat some doc in WorkloadRegistryEntry.

--- api/src/main/scala/twita/whipsaw/api/registry/WorkloadRegistryEntry.scala
+++ api/src/main/scala/twita/whipsaw/api/registry/WorkloadRegistryEntry.scala
@@ -10,44 +10,54 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
 /**
-  * Interface contract for an entry in the {{WorkloadRegistry}}.  When implementers develop new types of workloads,
-  * they also need to create corresponding {{WorkloadRegistryEntry}} instances so that these new types of workloads
+  * Describes an entry in the [[WorkloadRegistry]].  When implementers develop new types of workloads,
+  * they also need to create corresponding WorkloadRegistryEntry instances so that these new types of workloads
   * may be managed by the system.
   */
 trait WorkloadRegistryEntry {
+
   /**
-    * @return A {{WorkloadFactory}} instance that may be used to create a new instance of the {{Workload}} represented
-    *         by this entry.
+    * Defines a WorkloadFactory instance that may be used to create a new instance of the Workload represented
+    * by this entry.
+    *
+    * @return A WorkloadFactory instance
     */
   def factory: WorkloadFactory[_, _, _]
 
   /**
-    * @return The {{Metadata}} instance that corresponds to the {{Workload}} represented by this entry.  {{Workload}}
-    *         classes are generic over 3 types: the Payloads that are processed for each WorkItem, the Scheduler that
-    *         knows how to materialize all of the Payloads, and a Processor that will be used to process all of the
-    *         payloads.  The Metadata instance pulls all of these types together and provides a complete representation
-    *         of the application-specific processing that must happen for each type of Workload.
+    * Defines the Metadata instance that corresponds to the Workload represented by this entry.  Workload
+    * classes are generic over 3 types: the Payloads that are processed for each WorkItem, the Scheduler that
+    * knows how to materialize all of the Payloads, and a Processor that will be used to process all of the
+    * payloads.  The Metadata instance pulls all of these types together and provides a complete representation
+    * of the application-specific processing that must happen for each type of Workload.
+    *
+    * @return Metadata instance that describes this Workload
     */
   def metadata: Metadata[_, _, _]
 
   /**
-    * @param id The id of a {{Workload}} that has already been created in the system.
+    * Returns a Future that is to be completed by a Workload that has already been added to the system.
+    * Note that the generic types of the Workload are not known.  The idea here is that there are a handful
+    * of methods on Workload interface that may be executed without knowledge of what the actual concrete
+    * implementation's generic types are.  These methods are used by the Workload Engine to interact with the
+    * Workload, but the engine itself doesn't know or care what the generic types actually are.
+    *
+    * @param id The id of a Workload that has already been created in the system.
     * @param executionContext
-    * @return Asynchronously returns a {{Workload}} instance.  Note that the generic types of the Workload are not
-    *         known.  The idea here is that there are a handful of methods on {{Workload}} interface that may be
-    *         executed without knowledge of what the actual concrete implementation's generic types are.  These methods
-    *         are used by the workload engines to interact with the workload, but the engine itself doesn't know or
-    *         care what the generic types actually are.
+    * @return A Future to be completed by a {{Workload}} instance.
     */
-  def forWorkloadId(id: WorkloadId)(implicit executionContext: ExecutionContext): Future[Workload[_, _, _]]
+  def forWorkloadId(id: WorkloadId)(
+    implicit executionContext: ExecutionContext
+  ): Future[Workload[_, _, _]]
 
   /**
-    * @param md {{Metadata}} instance that we will use to look up the Workload factory.  Metadata instances are created
-    *           by developers as they code new types of {{Workload}} classes.  The {{Metadata}} instance that is
-    *           created as part of the development process should be created as a singleton that is accessible both in
-    *           the application that will be instantiating new workloads and in the engine that will be materializing
-    *           and executing the Workloads.
+    * Returns a WorkloadFactory given a Metadata instance.   Metadata instances are created by developers as they
+    * code new types of Workload classes.  The Metadata instance that is created as part of the development process
+    * should be created as a singleton that is accessible both in the application that will be originating new
+    * Workloads and in the Engine that will be materializing and executing the Workloads.  Unlike the
+    * [[WorkloadRegistryEntry.forWorkloadId()]], this method does in fact fill in the generic types for the Workload.
     *
+    * @param md {{Metadata}} instance that we will use to look up the Workload factory.
     * @param executionContext
     * @tparam Payload Arbitrary type that contains all of the information that is needed to process an individual
     *                 {{WorkItem}}.  The Payload is what makes the item unique in this Workload.  For example, if we
@@ -60,5 +70,7 @@ trait WorkloadRegistryEntry {
     */
   def factoryForMetadata[Payload: OFormat, SParams: OFormat, PParams: OFormat](
     md: Metadata[Payload, SParams, PParams]
-  )(implicit executionContext: ExecutionContext): WorkloadFactory[Payload, SParams, PParams]
+  )(
+    implicit executionContext: ExecutionContext
+  ): WorkloadFactory[Payload, SParams, PParams]
 }
